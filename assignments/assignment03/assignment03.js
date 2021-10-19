3562ee2f0f4a4480ccb378088ab9a094e22ac225
happyyellowdoggo <89933683+HappyYellowDoggo@users.noreply.github.com>
2021-10-18 23:54:17 -0400

    Update assignment03.js

--- assignments/assignment03/assignment03.js
+++ assignments/assignment03/assignment03.js
@@ -19,9 +19,12 @@ elem[16] = document.getElementById("16");
 elem[17] = document.getElementById("17");
 elem[18] = document.getElementById("18");
 
-elem[19] = document.getElementById("totals");
+// giving totals row an index
+elem[19] = document.getElementById("finalScores");
 
+// add up all par 4's
 elem[19].children[1].innerHTML = 72;
+
 // display the number of children (all td elements)
 // console.log(elem.children.length);
 // display the content of the + button, which is the first child of the fifth element
