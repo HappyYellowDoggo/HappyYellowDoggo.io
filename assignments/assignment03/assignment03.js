9488b053908c6fbd2eeb02ca75758a5a2ffbd600
happyyellowdoggo <89933683+HappyYellowDoggo@users.noreply.github.com>
2021-10-18 23:45:01 -0400

    Update assignment03.js

--- assignments/assignment03/assignment03.js
+++ assignments/assignment03/assignment03.js
@@ -19,15 +19,52 @@ elem[16] = document.getElementById("16");
 elem[17] = document.getElementById("17");
 elem[18] = document.getElementById("18");
 
+// give totals row an index
 elem[19] = document.getElementById("totals");
 
-elem[19].children[1].innerHTML = 72;
-// display the number of children (all td elements)
-// console.log(elem.children.length);
-// display the content of the + button, which is the first child of the fifth element
-// console.log(elem.children[4].children[0]); 
+// give scores button an index
+elem[20] = document.getElementById("finalScores")
 
-// assign a function to the + button
+
+// assign an index to the C button
+elem[1].children[4].children[2].onclick 
+  = function(){c1(elem[1]);};
+elem[2].children[4].children[2].onclick 
+  = function(){c1(elem[2]);};
+elem[3].children[4].children[2].onclick 
+  = function(){c1(elem[3]);};
+elem[4].children[4].children[2].onclick 
+  = function(){c1(elem[4]);};
+elem[5].children[4].children[2].onclick 
+  = function(){c1(elem[5]);};
+elem[6].children[4].children[2].onclick 
+  = function(){c1(elem[6]);};
+elem[7].children[4].children[2].onclick 
+  = function(){c1(elem[7]);};
+elem[8].children[4].children[2].onclick 
+  = function(){c1(elem[8]);};
+elem[9].children[4].children[2].onclick 
+  = function(){c1(elem[9]);};
+elem[10].children[4].children[2].onclick 
+  = function(){c1(elem[10]);};
+elem[11].children[4].children[2].onclick 
+  = function(){c1(elem[11]);};
+elem[12].children[4].children[2].onclick 
+  = function(){c1(elem[12]);};
+elem[13].children[4].children[2].onclick 
+  = function(){c1(elem[13]);};
+elem[14].children[4].children[2].onclick 
+  = function(){c1(elem[14]);};
+elem[15].children[4].children[2].onclick 
+  = function(){c1(elem[15]);};
+elem[16].children[4].children[2].onclick 
+  = function(){c1(elem[16]);};
+elem[17].children[4].children[2].onclick 
+  = function(){c1(elem[17]);};
+elem[18].children[4].children[2].onclick 
+  = function(){c1(elem[18]);};
+
+// assign an index to the + button
 elem[1].children[4].children[0].onclick 
   = function(){add1(elem[1]);};
 elem[2].children[4].children[0].onclick 
@@ -65,7 +102,7 @@ elem[17].children[4].children[0].onclick
 elem[18].children[4].children[0].onclick 
   = function(){add1(elem[18]);};
 
-//assign a function to the - button
+//assign an index to the - button
 elem[1].children[4].children[1].onclick 
   = function(){sub1(elem[1]);};
 elem[2].children[4].children[1].onclick 
@@ -103,43 +140,10 @@ elem[17].children[4].children[1].onclick
 elem[18].children[4].children[1].onclick 
   = function(){sub1(elem[18]);};
 
-// assign a function to the C button
-elem[1].children[4].children[2].onclick 
-  = function(){c1(elem[1]);};
-elem[2].children[4].children[2].onclick 
-  = function(){c1(elem[2]);};
-elem[3].children[4].children[2].onclick 
-  = function(){c1(elem[3]);};
-elem[4].children[4].children[2].onclick 
-  = function(){c1(elem[4]);};
-elem[5].children[4].children[2].onclick 
-  = function(){c1(elem[5]);};
-elem[6].children[4].children[2].onclick 
-  = function(){c1(elem[6]);};
-elem[7].children[4].children[2].onclick 
-  = function(){c1(elem[7]);};
-elem[8].children[4].children[2].onclick 
-  = function(){c1(elem[8]);};
-elem[9].children[4].children[2].onclick 
-  = function(){c1(elem[9]);};
-elem[10].children[4].children[2].onclick 
-  = function(){c1(elem[10]);};
-elem[11].children[4].children[2].onclick 
-  = function(){c1(elem[11]);};
-elem[12].children[4].children[2].onclick 
-  = function(){c1(elem[12]);};
-elem[13].children[4].children[2].onclick 
-  = function(){c1(elem[13]);};
-elem[14].children[4].children[2].onclick 
-  = function(){c1(elem[14]);};
-elem[15].children[4].children[2].onclick 
-  = function(){c1(elem[15]);};
-elem[16].children[4].children[2].onclick 
-  = function(){c1(elem[16]);};
-elem[17].children[4].children[2].onclick 
-  = function(){c1(elem[17]);};
-elem[18].children[4].children[2].onclick 
-  = function(){c1(elem[18]);};
+//adding up all par 4's
+
+elem[19].children[1].innerHTML = 72;
+
 
 // create an "add1" function
 function add1 (elem) {
