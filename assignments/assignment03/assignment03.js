d24e23de4a033dd3f3abc7ad3227da2d6a517fbc
happyyellowdoggo <89933683+HappyYellowDoggo@users.noreply.github.com>
2021-10-18 23:34:28 -0400

    Update assignment03.js

--- assignments/assignment03/assignment03.js
+++ assignments/assignment03/assignment03.js
@@ -20,15 +20,19 @@ elem[16] = document.getElementById("16");
 elem[17] = document.getElementById("17");
 elem[18] = document.getElementById("18");
 
+// giving the totals row an index
 elem[19] = document.getElementById("totals");
 
-elem[19].children[1].innerHTML = 72;
+//giving final scores button an element
+elem[20] = document.getElementById("finalscores")
+     
+
 // display the number of children (all td elements)
 // console.log(elem.children.length);
 // display the content of the + button, which is the first child of the fifth element
 // console.log(elem.children[4].children[0]); 
 
-// assign a function to the + button
+// assign a function to the + buttons
 elem[1].children[4].children[0].onclick 
   = function(){add1(elem[1]);};
 elem[2].children[4].children[0].onclick 
@@ -66,7 +70,7 @@ elem[17].children[4].children[0].onclick
 elem[18].children[4].children[0].onclick 
   = function(){add1(elem[18]);};
 
-//assign a function to the - button
+//assign a function to the - buttons
 elem[1].children[4].children[1].onclick 
   = function(){sub1(elem[1]);};
 elem[2].children[4].children[1].onclick 
@@ -142,7 +146,15 @@ elem[17].children[4].children[2].onclick
 elem[18].children[4].children[2].onclick 
   = function(){c1(elem[18]);};
 
-// create an "add1" function
+// adding up all the par 4's
+elem[19].children[1].innerHTML = 72;
+
+//assigning a function to add up final scores
+elem[20].onclick = function() {add()};
+
+
+
+// create an add 1 function
 function add1 (elem) {
   if(elem.children[2].innerHTML == "-") 
     elem.children[2].innerHTML = "1";
@@ -151,37 +163,87 @@ function add1 (elem) {
     currentScore = Number.parseInt(currentScore);
     elem.children[2].innerHTML = currentScore + 1;
   }
-  if(elem.children[2].innerHTML == "-")
-    elem.children[3].innerHTML = "-";
-  else {
-    let currentScore = elem.children[2].innerHTML;
-    let par = elem.children[1].innerHTML;
-    currentScore = Number.parseInt(currentScore);
-    par = Number.parseInt(par);
-    elem.children[3].innerHTML = currentScore - par;
-  }
 }
 
-// create a "sub1" function
+//create sub 1 function
 function sub1 (elem) {
-  if(elem.children[2].innerHTML == "1"||elem.children[2].innerHTML == "-") 
-    elem.children[2].innerHTML = "-";
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "-1";
   else {
     let currentScore = elem.children[2].innerHTML;
     currentScore = Number.parseInt(currentScore);
     elem.children[2].innerHTML = currentScore - 1;
   }
-  if(elem.children[2].innerHTML == "-")
-    elem.children[3].innerHTML = "-";
-  else {
-    let currentScore = elem.children[2].innerHTML;
-    let par = elem.children[1].innerHTML;
-    currentScore = Number.parseInt(currentScore);
-    par = Number.parseInt(par);
-    elem.children[3].innerHTML = currentScore - par;
-  }
 }
 
+//create add
+function add(){
+  addScore();
+  addOver();
+}
+
+// addScore
+function addScore (){
+  
+    for(let i = 1; i < 19; i++){
+    if(elem[i].children[2].innerHTML == "-"){
+      elem[i].children[2].innerHTML = 0;
+    }
+  }
+
+  elem[20].children[2].innerHTML = 
+   
+Number(elem[1].children[2].innerHTML) +                             
+Number(elem[2].children[2].innerHTML)+
+Number(elem[3].children[2].innerHTML)+
+Number(elem[4].children[2].innerHTML)+
+Number(elem[5].children[2].innerHTML)+
+Number(elem[6].children[2].innerHTML)+
+Number(elem[7].children[2].innerHTML)+
+Number(elem[8].children[2].innerHTML)+
+Number(elem[9].children[2].innerHTML)+
+Number(elem[10].children[2].innerHTML)+
+Number(elem[11].children[2].innerHTML)+
+Number(elem[12].children[2].innerHTML)+
+Number(elem[13].children[2].innerHTML)+
+Number(elem[14].children[2].innerHTML)+
+Number(elem[15].children[2].innerHTML)+
+Number(elem[16].children[2].innerHTML)+
+Number(elem[17].children[2].innerHTML)+
+Number(elem[18].children[2].innerHTML);
+}
+
+// addover
+function addOver (){
+  
+   for(let i = 1; i < 19; i++){
+    if(elem[i].children[3].innerHTML == "-"){
+      elem[i].children[3].innerHTML = 0;
+    }
+  }
+  elem[20].children[3].innerHTML = 
+   
+Number(elem[1].children[3].innerHTML) +                             
+Number(elem[2].children[3].innerHTML)+
+Number(elem[3].children[3].innerHTML)+
+Number(elem[4].children[3].innerHTML)+
+Number(elem[5].children[3].innerHTML)+
+Number(elem[6].children[3].innerHTML)+
+Number(elem[7].children[3].innerHTML)+
+Number(elem[8].children[3].innerHTML)+
+Number(elem[9].children[3].innerHTML)+
+Number(elem[10].children[3].innerHTML)+
+Number(elem[11].children[3].innerHTML)+
+Number(elem[12].children[3].innerHTML)+
+Number(elem[13].children[3].innerHTML)+
+Number(elem[14].children[3].innerHTML)+
+Number(elem[15].children[3].innerHTML)+
+Number(elem[16].children[3].innerHTML)+
+Number(elem[17].children[3].innerHTML)+
+Number(elem[18].children[3].innerHTML);
+}
+
+  
 function c1 (elem) {
   elem.children[2].innerHTML = "-";
   elem.children[3].innerHTML = "-";
